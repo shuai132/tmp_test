cc2226d61479383829af7730c3e813c24893fced
liushuai <770722922@qq.com>
2023-04-04 19:20:36 +0800

    msg

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X POST -F 'file=@tmp.zip' http://1254a01d.r6.cpolar.top/upload
+echo "upload start!!!"
+curl -X POST -F 'file=@tmp.zip' http://39c72c05.r6.cpolar.top/upload
+echo "upload done!!!"
